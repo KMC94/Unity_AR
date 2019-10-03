190d7d461681246cdfdd22678e20a6fecdd6a41a
kmc94 <money94611@naver.com>
2019-10-03 11:52:53 +0900

    AR_Day1 [Configuration]

---
+++ SolarSystem/Assets/Scripts/RotatePlanet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+
+    public Transform targetTr;
+    public float rotSpeed = 15.0f;
+
+    Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();    
+    }
+
+    void Update()
+    {
+        tr.RotateAround(targetTr.position, Vector3.up, Time.deltaTime * rotSpeed);
+    }
+}
